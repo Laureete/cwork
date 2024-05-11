ecdea146e175f3fcac0521995baff2beec1a1cd6
Laureete <ongwesolaura@gmail.com>
2024-05-11 15:31:15 +0300

    were

--- mini_karrot/lib/utils/constants/text_strings.dart
+++ mini_karrot/lib/utils/constants/text_strings.dart
@@ -10,7 +10,7 @@ class TTexts {
   static const String appName = "mini_karrots";
   static const String tContinue = "Continue";
   //NEW COMMIT LAB COMP
-  //add
+  //addwere
 
 
   // -- OnBoarding Texts
