321fa08468bbce777ac22b5dbb448fb67beaa878
Laureete <ongwesolaura@gmail.com>
2024-05-11 15:25:03 +0300

    main

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -15,6 +15,7 @@ class TTexts {
   static const String onBoardingTitle1 = "Choose your product";
   static const String onBoardingTitle2 = "Select Payment Method";
   static const String onBoardingTitle3 = "Deliver at your door step";
+  //main
 
   static const String onBoardingSubTitle1 = "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
   static const String onBoardingSubTitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
